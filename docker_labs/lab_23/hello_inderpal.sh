5346d3d229649c983456a509e0bb147bf892b3ea
Inderpal Singh <inderpal-singh.saini@capgemini.com>
2019-10-18 14:52:15 +0200

    Added lab 23 files.

---
+++ docker_labs/lab_23/hello_inderpal.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Hello Inderpal Singh Saini."
+echo "If you see this text on running the container from the image inderpal2406/hello-inderpal, then it means we have created a similar image to hello-world provided by docker."
+echo "Good Luck!"
